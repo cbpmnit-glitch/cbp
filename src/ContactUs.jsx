e682d15b5b3986125223a905ae65f1ec5511be81
cbpmnit-glitch <cbpmnit@gmail.com>
2025-08-19 22:58:43 +0530

    13th commit

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Navbar from './navbar'
-
+import Navbar from './navbar';
 const ContactUs = () => {
-  <Navbar/>
   return (
-    
+    <>
+      <Navbar />
     <div id="ContactUs" className="p-6">
       <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
       <p className="mb-2">Last updated on Aug 17th 2025</p>
@@ -27,6 +26,7 @@ const ContactUs = () => {
         <strong>E-Mail ID:</strong> cbpmnit@gmail.com
       </p>
     </div>
+    </>
   )
 }
 
