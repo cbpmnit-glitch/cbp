28303de616a942d11e5cd70813541209dba6c072
cbpmnit-glitch <cbpmnit@gmail.com>
2025-08-19 21:04:26 +0530

    6th commit

--- src/footer.jsx
+++ src/footer.jsx
@@ -86,11 +86,10 @@ const App = () => {
               <p className="font-bold mb-2">Community</p>
               <ul className="space-y-1">
                 <li><a href="/TermsAndConditions.html">Terms & Conditions</a></li>
-                <li>Meet The Team</li>
-                <li>Satvic Stories</li>
-                <li>Blogs</li>
-                <li>Contact Us</li>
-                <li>Workshop Login</li>
+                <li><a href="/privacy policy.html">Privacy policies</a></li>
+                <li><a href="shipping policy">Shipping Policy</a></li>
+                <li><a href="/Contact us.html">Contact Us</a></li>
+                <li><a href="/Cancellation & Refund.html">Cancellation & Refund</a></li>
               </ul>
             </div>
           </motion.div>
