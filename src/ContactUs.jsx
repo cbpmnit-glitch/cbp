6670fbb0a16040bce71a83d58a78f95844a50782
cbpmnit-glitch <cbpmnit@gmail.com>
2025-08-19 23:08:52 +0530

    14th commit

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './navbar';
-import { Footer } from 'framer-motion/client';
+import Footer from '../components/Footer'; 
 const ContactUs = () => {
   return (
     <>
