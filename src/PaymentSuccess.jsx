cc987c6c7121c611ff6ce815f84124bbc997ac22
cbpmnit-glitch <cbpmnit@gmail.com>
2025-08-26 06:41:42 +0530

    Update PaymentSuccess.jsx

--- src/PaymentSuccess.jsx
+++ src/PaymentSuccess.jsx
@@ -4,7 +4,6 @@ const PaymentSuccess = () => (
   <div style={{ textAlign: "center", marginTop: "50px" }}>
     <h1>✅ Payment Successful</h1>
     <p>Your registration has been submitted successfully.</p>
-    <a href="/attendance">Go to Attendance System</a>
   </div>
 );
 
